8f6bfc80328782373f44364d6f29c28fa078f688
IgnisRT7 <35602066+IgnisRT7@users.noreply.github.com>
2018-09-26 16:02:43 +0900

    不具合修正 Title::operator() : エンティティの描画順を Other から BackGroundに変更し透過情報を正しく描画可能に

--- OpenGLPuzzleCubeProject/src/GameState.cpp
+++ OpenGLPuzzleCubeProject/src/GameState.cpp
@@ -22,18 +22,29 @@ namespace GameState {
 		game.Camera({ glm::vec4(0,20,-8,1),glm::vec3(0,0,12),glm::vec3(0,0,1) });
 
 		if (!pSpaceSphere) {
-			pSpaceSphere = game.AddEntity(EntityGroupId_Others, glm::vec3(0, 0, 0), "SpaceSphere", "Res/SpaceSphere.bmp", &UpdateSpaceSphere, "NonLighting");
+			pSpaceSphere = game.AddEntity(EntityGroupId_Background, glm::vec3(0, 0, 0), "SpaceSphere", "Res/SpaceSphere.bmp", &UpdateSpaceSphere, "NonLighting");
 		}
 
 		const float offset = timer == 0 ? 0 : 0 (2.0f - timer) * (2.0f - timer) * 2.0f;
+		game.FontColor(glm::vec4(1, 1, 1, 1));
 		game.FontScale(glm::vec2(2));
 		game.AddString(glm::vec2(-0.25f + offset, 0.125f), "STAR FIGHTER");
 		game.FontScale(glm::vec2(0.5f));
 		game.AddString(glm::vec2(0.1f + offset, -0.05f), "The ultimate in Manned-Fighter");
 
+		static float tmpTimer= 0;
+		tmpTimer += delta;
+		auto f = (sinf(glm::radians((float)tmpTimer)) + 1) / 2;
+
+		game.FontColor(glm::vec4(1.0f,0, 0, 0));
+		game.AddString(glm::vec2(-0.2, -0.5), "Pressed Enter...");
+
+
 		if (timer > 0) {
 			timer -= static_cast<float>(delta);
 			if (timer <= 0) {
+
+				//タイトル画面からメインゲーム画面の更新処理へ移行
 				game.UpdateFunc(MainGame(pSpaceSphere));
 			}
 		}
