b34aaabbf18ad86486179ad216af7689c056402a
IgnisRT7 <35602066+IgnisRT7@users.noreply.github.com>
2019-05-06 22:56:55 +0900

    ゲーム終了時のシーンを追加

---
+++ OpenGLPuzzleCubeProject/src/user/GameEndScene.cpp
@@ -0,0 +1,29 @@
+/**
+*	@file GameEndScene.cpp
+*/
+#include "GameEndScene.h"
+
+namespace GameState{
+
+	/**
+	*	コンストラクタ
+	*/
+	GameEnd::GameEnd() {
+
+	}
+
+	/**
+	*	初期化処理
+	*/
+	void GameState::GameEnd::Initialize()
+	{
+	}
+
+	/**
+	*	更新処理
+	*/
+	void GameEnd::operator()(double delta)
+	{
+	}
+
+}
---
+++ OpenGLPuzzleCubeProject/src/user/GameEndScene.h
@@ -0,0 +1,26 @@
+/**
+*	@file GameEndScene.h
+*/
+#pragma once
+
+#include "TitleScene.h"
+
+namespace GameState{
+
+	///ƒ^ƒCƒgƒ‹‰æ–Ê
+	class GameEnd {
+	public:
+
+		GameEnd();
+
+		void Initialize();
+
+		void operator()(double delta);
+
+	private:
+
+		float timer = 0;
+
+		TitleSpaceSphere spaceSphere;
+	};
+}
