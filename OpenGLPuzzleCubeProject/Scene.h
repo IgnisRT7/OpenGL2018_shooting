8ad38ab03b2076498ad07a8f1ca92edda2b523b5
IgnisRT7 <35602066+IgnisRT7@users.noreply.github.com>
2019-05-12 17:51:22 +0900

    シーン管理用クラスの追加

---
+++ OpenGLPuzzleCubeProject/Scene.h
@@ -0,0 +1,71 @@
+/**
+*	@Scene.h
+*/
+
+#pragma once
+#include <memory>
+#include <string>
+#include <vector>
+
+/**
+*	シーンクラス
+*/
+class Scene {
+public:
+
+	Scene(const char* name);
+	Scene(const Scene&) = delete;
+	Scene& operator=(const Scene&) = delete;
+	virtual ~Scene() {}
+
+	virtual bool Initialize() = 0 {};
+	virtual void Update(float delta) = 0 {};
+	virtual void Render() = 0 {};
+	virtual void Finalize() = 0 {};
+
+	virtual void Play();
+	virtual void Stop();
+	virtual void Hide();
+
+	const std::string Name() const;
+
+protected:
+
+	std::string name;		///< シーンの名前
+	bool isActive = true;	///< アクティブ状態かどうか
+	bool isVisible = true;	///< 表示中かどうか
+};
+
+using ScenePtr = std::shared_ptr<Scene>;
+
+/**
+*	シーン管理クラス
+*/
+class SceneStack {
+public:
+
+	static SceneStack& Instance();
+
+	void PushScene(ScenePtr);
+	void Pop();
+	void Replace(ScenePtr);
+	Scene& Current();
+	const Scene& Current()const;
+	size_t Size() const;
+	bool Empty() const;
+
+	void Update(float);
+	void Render();
+
+private:
+
+	SceneStack();
+	SceneStack(const SceneStack&) = delete;
+	SceneStack& operator=(const SceneStack&) = delete;
+	~SceneStack() = default;
+
+	std::vector<ScenePtr> stack;
+
+	std::vector<ScenePtr> buffer;
+
+};
