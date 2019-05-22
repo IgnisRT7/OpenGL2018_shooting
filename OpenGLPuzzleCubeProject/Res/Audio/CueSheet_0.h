c7c23f28f691bf149903588265be845d5b069e3d
IgnisRT7 <35602066+IgnisRT7@users.noreply.github.com>
2019-05-22 19:32:29 +0900

    サウンドデータの更新

---
+++ OpenGLPuzzleCubeProject/Res/Audio/CueSheet_0.h
@@ -0,0 +1,38 @@
+﻿/*===========================================================================*
+ *  Header file for Atom CueSheet Binary
+ *  Project          : testProject
+ *  Tool Ver.        : Ver.1.02.00
+ *  File Path        : D:/VisualStudio/Projects/OpenGLPuzzleCubeProject_test/OpenGLPuzzleCubeProject/Res/Audio/Public/WorkUnit_0
+ *  File Name        : CueSheet_0.h
+ *  File Size        : 9296672 bytes
+ *  Date Time        : 2019/05/22 19:03:39
+ *  Target           : Public
+ *  Cues             : 14
+ *  CueSheet Comment : 
+ *  Stream Awb Path  : D:/VisualStudio/Projects/OpenGLPuzzleCubeProject_test/OpenGLPuzzleCubeProject/Res/Audio/Public/WorkUnit_0/CueSheet_0.awb
+ *===========================================================================*/
+
+#define CRI_CUESHEET_0_CUENUM (14)
+
+/* AISAC Control List (AISAC Control ID) */
+
+/* Cue List (Cue ID) */
+#define CRI_CUESHEET_0_GAMEMAIN             ( 0) /*  */
+#define CRI_CUESHEET_0_GAMEOVER             ( 1) /*  */
+#define CRI_CUESHEET_0_SELECT               ( 2) /*  */
+#define CRI_CUESHEET_0_SHOT                 ( 3) /*  */
+#define CRI_CUESHEET_0_TITLE1               ( 4) /*  */
+#define CRI_CUESHEET_0_TITLE2               ( 5) /*  */
+#define CRI_CUESHEET_0_AIRCRAFTSELECT       ( 6) /*  */
+#define CRI_CUESHEET_0_BOSSSCENE            ( 7) /*  */
+#define CRI_CUESHEET_0_ENEMYSHOT            ( 8) /*  */
+#define CRI_CUESHEET_0_EXPLOSIVE            ( 9) /*  */
+#define CRI_CUESHEET_0_EXPLOSIVE2           (10) /*  */
+#define CRI_CUESHEET_0_MAIN2SCENE           (11) /*  */
+#define CRI_CUESHEET_0_MAINSCENE            (12) /*  */
+#define CRI_CUESHEET_0_SELECT2              (13) /*  */
+
+/* Block List (Block Index) */
+
+/* end of file */
+
