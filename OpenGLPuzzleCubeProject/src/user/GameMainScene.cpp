49afc19e82a10690998d38349dde9ab42cd7b095
拓也 横山 <ignisrt7mssp@gmail.com>
2019-06-04 10:56:31 +0900

    ライティング位置をステージ繊維時に変更した

--- OpenGLPuzzleCubeProject/src/user/GameMainScene.cpp
+++ OpenGLPuzzleCubeProject/src/user/GameMainScene.cpp
@@ -197,6 +197,16 @@ namespace GameState {
 
 			game.KeyValue(0.24f);
 
+			///ƒVƒƒƒhƒE‚Ìİ’è
+			GameEngine::ShadowParameter shadowParam;
+			shadowParam.lightPos = glm::vec3(20, 50, 50);
+			shadowParam.lightDir = glm::normalize(glm::vec3(-25, -50, 25));
+			shadowParam.lightUp = glm::vec3(0, 1, 0);
+			shadowParam.near = 10;
+			shadowParam.far = 200;
+			shadowParam.range = glm::vec2(500, 500);
+			game.Shadow(shadowParam);
+
 			//”wŒi‚Ì‰Šú‰»ˆ—
 			for (int z = 0; z < 5; ++z) {
 				const float offsetZ = static_cast<float>(z * 40);
