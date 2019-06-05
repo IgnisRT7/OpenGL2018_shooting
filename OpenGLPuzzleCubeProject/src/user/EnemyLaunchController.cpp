8feaa94795dacfe0cbc5053df417d708d883864c
IgnisRT7 <35602066+IgnisRT7@users.noreply.github.com>
2019-06-05 18:13:00 +0900

    クラスの実装を行った

---
+++ OpenGLPuzzleCubeProject/src/user/EnemyLaunchController.cpp
@@ -0,0 +1,81 @@
+/**
+*	@file EnemyLaunchController.cpp
+*/
+
+#include "EnemyLaunchController.h"
+#include <algorithm>
+#include "Entity/Enemy.h"
+#include "../GameEngine.h"
+#include "../GameState.h"
+
+EnemyLaunchType stage1EnemyLaunchList[] = {
+	/// count,interval,type,time,position
+	{ 5,	1,	1,	2,	{ -100, 0, 199 } },
+	{ 5,	1,	1,	2,	{ 100 , 0, 199 } },
+	{ 5,	1,	1,	5,	{ -100, 0, 199 } },
+	{ 5,	1,	1,	5,	{ 100 , 0, 199 } },
+	{ 5,	1,	1,	7,	{ -100, 0, 199 } },
+	{ 5,	1,	1,	7,	{ 100 , 0, 199 } },
+};
+
+/**
+*	初期化処理
+*
+*	@param stageNum	ロードするステージの番号
+*/
+void EnemyLaunchController::Init(int stageNum){
+
+	for (auto& launch : stage1EnemyLaunchList) {
+
+		launchList.push_back(launch);
+	}
+	std::sort(launchList.begin(), launchList.end());
+}
+
+/**
+*	更新処理
+*
+*	@param deltaTime	経過時間
+*/
+void EnemyLaunchController::Update(float deltaTime){
+
+	timer += deltaTime;
+
+	//現在の参照位置の出撃データを取得する
+	auto& itr = (launchList.begin() + seekIndex);
+
+	while (true) {
+
+		if (seekIndex >= launchList.size()) {
+			isFinished = true;
+			break;
+		}
+
+		if (itr->launchStartTimer < timer) {
+			//出撃可能な状態になった
+
+			auto e = std::make_shared<GameState::EnemySpawner>(
+				itr->launchCount,itr->launchInterval,itr->enemyType);
+			GameEngine::Instance().AddEntity(
+				GameState::EntityGroupId_Others, itr->startPostion, nullptr, nullptr, e);
+			std::cout << "EnemySpawner is Launched index: " << seekIndex << " time: "<<timer << std::endl;
+
+
+			//次の出撃データを確認する
+			itr++;
+			seekIndex++;
+		}
+		else {
+			break;
+		}
+	}
+}
+
+void EnemyLaunchController::Launch(){
+
+
+
+//	GameEngine::Instance().AddEntity(
+//		GameState::EntityGroupId_Others,activeList[0].startPostion,nullptr,nullptr,
+//	)
+}
--- OpenGLPuzzleCubeProject/src/user/EnemyLaunchController.h
+++ OpenGLPuzzleCubeProject/src/user/EnemyLaunchController.h
@@ -3,13 +3,23 @@
 */
 
 #include <vector>
+#include <glm/glm.hpp>
+#include "../Entity.h"
 
 struct EnemyLaunchType {
 
-	int launchCount;	///< 出撃数
+	int launchCount;		///< 出撃数
 	float launchInterval;	///< 出撃間隔
-	int enemyType;		///< 敵の種類
+	int enemyType;			///< 敵の種類
 	float launchStartTimer;	///< ステージ開始後に出撃する時間
+	glm::vec3 startPostion;	///< 初期地点
+
+	bool operator>(const EnemyLaunchType& cmp) const {
+		return launchStartTimer > cmp.launchStartTimer;
+	}
+	bool operator<(const EnemyLaunchType& cmp) const { 
+		return launchStartTimer < cmp.launchStartTimer;
+	}
 };
 
 /**
@@ -18,19 +28,25 @@ struct EnemyLaunchType {
 class EnemyLaunchController {
 public:
 
-	EnemyLaunchController();
-	~EnemyLaunchController();
+	EnemyLaunchController() = default;
+	~EnemyLaunchController() {}
 	EnemyLaunchController(const EnemyLaunchController&) = delete;
 	void operator=(const EnemyLaunchController&) = delete;
 
 	void Init(int stageNum);
 
-	void Update(float delta);
+	void Update(float deltaTime);
+	void Launch();
+	bool IsFinish() { return isFinished; }
 
 private:
 
 	std::vector<EnemyLaunchType> launchList;
-
-
+	std::vector<EnemyLaunchType> activeList;
+	int seekIndex = 0;
+	float timer = 0;
+	bool isFinished;
 
 };
+
+using EnemyLaunchControllerPtr = std::shared_ptr<EnemyLaunchController>;
