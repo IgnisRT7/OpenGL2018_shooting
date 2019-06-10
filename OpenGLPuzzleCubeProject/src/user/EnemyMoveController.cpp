f0fa604a0ceb65992063ce535371e5412eae8cb7
IgnisRT7 <35602066+IgnisRT7@users.noreply.github.com>
2019-06-10 20:31:06 +0900

    敵用の移動制御ファイルの作成とクラスの実装

---
+++ OpenGLPuzzleCubeProject/src/user/EnemyMoveController.cpp
@@ -0,0 +1,164 @@
+/**
+*	@file EnemyMoveController.cpp
+*/
+#include "EnemyMoveController.h"
+
+MovePart::MovePart(float d) {
+	duration = d;
+}
+
+/**
+*	制御するトウィーニングオブジェクトを設定する
+*
+*	@param p	トウィーニングオブジェクトへのポインタ
+*/
+MoveController::MoveController(const MovePartPtr& p):
+part(p),elapsedTime(0.f){
+}
+
+/**
+*	更新処理
+*
+*	@param entity	制御するエンティティ
+*	@param delta	経過時間
+*/
+void MoveController::Update(Entity::Entity& entity, float delta) {
+
+	if (!part) {
+		//未設定なので処理しない
+		return;
+	}
+
+	
+	if(!isInitialized){
+		isInitialized=true;
+		part->Initialize(entity);
+	}
+	
+	float durationTime = part->Duration();
+	elapsedTime += delta;
+	if (elapsedTime < durationTime) {
+		//動作期間内
+
+		part->Update(entity,elapsedTime);
+	}
+	else {
+		//動作期間外
+
+		part->Update(entity, durationTime);
+
+		if (isLoop) {
+
+			part->Initialize(entity);
+			elapsedTime -= durationTime;
+			part->Update(entity, elapsedTime);
+		}
+
+	}
+}
+
+/**
+*	コンストラクタ
+*
+*	@param d	動作時間
+*	@param v	移動距離
+*/
+MoveStraight::MoveStraight(float d, const glm::vec3& v) :
+	moveVel(v),MovePart(d){
+}
+
+/**
+*	初期化処理
+*
+*	@param entity	制御するエンティティ
+*/
+void MoveStraight::Initialize(Entity::Entity& entity) {
+
+	MovePart::Initialize(entity);
+	start = entity.Position();
+}
+
+/**
+*	更新処理
+*
+*	@param entity		制御するエンティティ
+*	@param elapsedTime	経過時間
+*/
+void MoveStraight::Update(Entity::Entity& entity, float elapsedTime) {
+
+	const float ratio = glm::clamp(elapsedTime / duration, 0.f, 1.f);
+	glm::vec3 newPos = start + moveVel * ratio;
+	entity.Position(newPos);
+}
+
+/**
+*	初期化処理
+*
+*	@param entity	制御対象のエンティティ
+*/
+void MoveControllSequencer::Initialize(Entity::Entity& entity) {
+
+	MovePart::Initialize(entity);
+	execIndex = -1;
+	currentTimeBegin = currentTimeEnd = 0;
+	NextMove(entity);
+}
+
+/**
+*	更新処理
+*
+*	@param entity		制御対象のエンティティ
+*	@param elapsedTime	経過時間
+*/
+void MoveControllSequencer::Update(Entity::Entity& entity, float elapsedTime){
+
+	if (execIndex < 0 || execIndex >= movList.size()) {
+		//実行対象が存在しない
+		return;
+	}
+
+	while (elapsedTime >= currentTimeEnd) {
+		//
+
+		movList[execIndex]->Update(entity, movList[execIndex]->Duration());
+		if (!NextMove(entity)) {
+			return;
+		}
+	}
+
+	movList[execIndex]->Update(entity, elapsedTime - currentTimeBegin);
+}
+
+/**
+*	動作リストに追加
+*
+*	@param p	追加する動作パーツ
+*/
+void MoveControllSequencer::Add(const MovePartPtr p){
+
+	movList.push_back(p);
+	duration += p->Duration();
+}
+
+/**
+*	次に実行させる動作パーツの設定をする
+*
+*	@param entity	制御対象のエンティティ
+*
+*	@retval true	次に動作させるパーツが存在し設定した
+*	@retval false	次に動作させるパーツが存在しない(last index)
+*/
+bool MoveControllSequencer::NextMove(Entity::Entity& entity) {
+
+	if (execIndex >= static_cast<int>(movList.size()) - 1) {
+		return false;
+	}
+
+	execIndex++;
+
+	currentTimeBegin = currentTimeEnd;
+	currentTimeEnd += movList[execIndex]->Duration();
+	movList[execIndex]->Initialize(entity);
+
+	return true;
+}
---
+++ OpenGLPuzzleCubeProject/src/user/EnemyMoveController.h
@@ -0,0 +1,104 @@
+/**
+*	@file EnemyMoveController.h
+*/
+
+#pragma once
+#include "../Entity.h"
+
+/**
+*	移動制御のパーツクラス
+*	tips:	エンティティの動作とその動作にかかる時間などを管理する
+*/
+class MovePart {
+public:
+
+	MovePart() = default;
+	MovePart(float d);
+	MovePart& operator=(const MovePart&) = delete;
+	virtual ~MovePart() = default;
+
+	float Duration() const { return duration; }
+	void Duration(float t) { duration = t; }
+
+	virtual void Initialize(Entity::Entity& e) { }
+	virtual void Update(Entity::Entity& e,float d) = 0;
+
+
+
+protected:
+
+	//MovePart* parallelExec;	///< 同時進行させる
+	
+	float duration;		///< 経過時間
+};
+
+using MovePartPtr = std::shared_ptr<MovePart>;
+
+/**
+*	移動制御クラス
+*/
+class MoveController {
+public:
+
+	MoveController() = default;
+	explicit MoveController(const MovePartPtr& p);
+	MoveController(const MoveController&) = delete;
+	MoveController& operator=(const MoveController&) = delete;
+
+	void Update(Entity::Entity& entity, float delta);
+
+private:
+
+	MovePartPtr part;	///< 制御する動作クラス
+	float elapsedTime;	///< 経過時間
+
+	bool isInitialized = false;	///< 初期化フラグ
+	bool isLoop = false;		///< ループ動作フラグ
+};
+
+using MoveControllerPtr = std::shared_ptr<MoveController>;
+
+/**
+*	まっすぐに進む
+*/
+class MoveStraight : public MovePart {
+public:
+
+	MoveStraight() = default;
+	MoveStraight(float d,const glm::vec3& v);
+
+	MoveStraight(const MoveStraight&) = delete;
+	MoveStraight& operator=(const MoveStraight&) = delete;
+	virtual ~MoveStraight() = default;
+
+	virtual void Initialize(Entity::Entity& p) override;
+	void Update(Entity::Entity& e,float d) override;
+
+private:
+
+	glm::vec3 start;	///< スタート地点(エンティティ初期位置)
+	glm::vec3 moveVel;	///< スタートからの移動量
+	float ratio;		///< 現在の進行状態 1になると完了
+};
+
+class MoveControllSequencer : public MovePart{
+public:
+
+	MoveControllSequencer() = default;
+	MoveControllSequencer(const MoveControllSequencer&) = delete;
+	MoveControllSequencer& operator=(const MoveControllSequencer&) = delete;
+	virtual ~MoveControllSequencer() = default;
+
+	virtual void Initialize(Entity::Entity& p) override;
+	virtual void Update(Entity::Entity& p, float d) override;
+	void Add(const MovePartPtr p);
+
+private:
+
+	bool NextMove(Entity::Entity&);
+
+	std::vector<MovePartPtr> movList;	///< 動作パーツのリスト
+	int execIndex = -1;					///< 現在実行中の動作パーツ
+	float currentTimeBegin;				///< 実行中の動作パーツの開始時間
+	float currentTimeEnd;				///< 実行中の動作パーツの終了時間
+};
