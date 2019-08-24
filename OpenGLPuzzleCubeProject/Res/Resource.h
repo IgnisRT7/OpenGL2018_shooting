36f9a6b1ce5cb0fd55f5997625fb2c17c493bc1a
拓也 横山 <ignisrt7mssp@gmail.com>
2019-08-24 22:42:38 +0900

    リソース管理用ファイルの追加

---
+++ OpenGLPuzzleCubeProject/Res/Resource.h
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace Resource {
+
+	const char* titleName = "STAR FIGHTER";
+
+//画像ファイル
+
+	const char* tex_defaultNormal = "Res/Model/Dummy.Normal.bmp";
+	const char* tex_font = "Res/Font.fnt";
+
+
+//モデルファイル
+
+	
+//音声ファイル
+	const char* audioAcfFile = "Res/Audio/testProject.acf";
+	const char* audioAcbFile = "Res/Audio/CueSheet_0.acb";
+	const char* audioAwbFile = "Res/Audio/CueSheet_0.awb";
+
+//シェーダファイル
+
+}
